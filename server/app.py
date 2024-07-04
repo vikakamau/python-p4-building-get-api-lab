0fecd8ab99b9e848e2c63fb1f1f69187a2f835ae
victor kamau <vikakamau04@gmail.com>
2024-07-04 15:41:16 +0300

    final

--- server/app.py
+++ server/app.py
@@ -20,19 +20,65 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries =[]
+    for bakery in Bakery.query.all():
+        backery_dict ={
+            'id': bakery.id,
+            'name': bakery.name,
+            'created_at': bakery.created_at.isoformat()
+        }
+        bakeries.append(backery_dict)
+    response = make_response(
+        jsonify(bakeries),
+        200
+    )
+    return response   
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    if bakery:
+        bakery_dict = { 
+            'id': bakery.id,
+            'name': bakery.name,
+            'created_at': bakery.created_at.isoformat()
+        }
+        response = make_response(
+            bakery_dict,
+            200
+        )
+        return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = []
+    for baked_good in BakedGood.query.all():
+        baked_good_dict = {
+            'id': baked_good.id,
+            'name': baked_good.name,
+            'price': baked_good.price,
+            'created_at': baked_good.created_at.isoformat()
+        }
+        baked_goods.append(baked_good_dict)
+        response = make_response(
+            jsonify(baked_goods),
+            200
+            )
+        return response
+
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
-
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive:
+        return jsonify({
+            'id': most_expensive.id,
+            'name': most_expensive.name,
+            'price': most_expensive.price,
+            'created_at': most_expensive.created_at.isoformat(),
+            'bakery_id': most_expensive.bakery_id
+        }), 200
+    else:
+        return jsonify({'message': 'No baked goods found'}), 404
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
